ab198ce4c73c0339c5edd0cad414358601d8ea99
danielh4t <danielh4t@pm.me>
2022-08-23 17:31:36 +0200

    add coil-kt and internet permission

--- app/src/main/java/app/stacq/plan/CreateActivity.kt
+++ app/src/main/java/app/stacq/plan/CreateActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import app.stacq.plan.databinding.ActivityCreateBinding
+import coil.load
 
 class CreateActivity : AppCompatActivity() {
 
