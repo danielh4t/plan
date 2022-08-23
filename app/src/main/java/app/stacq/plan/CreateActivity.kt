7249bbee594693dc1996437199c30daae18ea099
danielh4t <danielh4t@pm.me>
2022-08-23 16:25:39 +0200

    add create activity

---
+++ app/src/main/java/app/stacq/plan/CreateActivity.kt
@@ -0,0 +1,11 @@
+package app.stacq.plan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
