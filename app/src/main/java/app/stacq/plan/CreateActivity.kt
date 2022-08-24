4b2fcecd77eaf22492f8294369fa5ed805b2c77e
danielh4t <danielh4t@pm.me>
2022-08-24 18:26:29 +0200

    add placeholder colors and random images

--- app/src/main/java/app/stacq/plan/CreateActivity.kt
+++ app/src/main/java/app/stacq/plan/CreateActivity.kt
@@ -2,7 +2,6 @@ package app.stacq.plan
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -10,6 +9,8 @@ import app.stacq.plan.data.model.Category
 import app.stacq.plan.data.model.Task
 import app.stacq.plan.data.source.local.PlanDatabase
 import app.stacq.plan.data.source.local.task.TasksLocalDataSource
+import app.stacq.plan.data.source.remote.IMAGES_MAX
+import app.stacq.plan.data.source.remote.REMOTE_ENDPOINT
 import app.stacq.plan.data.source.repository.TasksRepository
 import app.stacq.plan.databinding.ActivityCreateBinding
 import app.stacq.plan.ui.tasks.TasksViewModel
@@ -30,7 +31,8 @@ class CreateActivity : AppCompatActivity() {
 
         val tasksRepository = TasksRepository(localDataSource, Dispatchers.Main)
         val taskViewModelFactory = TasksViewModelFactory(tasksRepository)
-        val tasksViewModel = ViewModelProvider(this, taskViewModelFactory)[TasksViewModel::class.java]
+        val tasksViewModel =
+            ViewModelProvider(this, taskViewModelFactory)[TasksViewModel::class.java]
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create)
         binding.lifecycleOwner = this
@@ -43,10 +45,31 @@ class CreateActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
-
-        binding.createImage.load("https://plan-node-api.herokuapp.com/images/image.png") {
-            placeholder(R.color.purple_500)
+        val seed = (0..IMAGES_MAX).random()
+        val image = imageUrl(seed)
+        val placeholder = placeHolder(seed)
+        binding.createImage.load(image) {
+            crossfade(true)
+            placeholder(placeholder)
         }
-
     }
+
+    private fun imageUrl(seed: Int): String {
+        val endpoint = REMOTE_ENDPOINT
+        val file = "$seed.png"
+        return "$endpoint/images/$file"
+    }
+
+    private fun placeHolder(seed: Int): Int {
+        return when (seed) {
+            1 -> R.color.placeholder_1
+            2 -> R.color.placeholder_2
+            3 -> R.color.placeholder_3
+            4 -> R.color.placeholder_4
+            5 -> R.color.placeholder_5
+            else -> R.color.placeholder_0
+        }
+    }
+
+
 }
---
+++ app/src/main/java/app/stacq/plan/data/source/remote/RemoteConstants.kt
@@ -0,0 +1,4 @@
+package app.stacq.plan.data.source.remote
+
+const val IMAGES_MAX: Int = 5
+const val REMOTE_ENDPOINT = "https://plan-node-api.herokuapp.com"
