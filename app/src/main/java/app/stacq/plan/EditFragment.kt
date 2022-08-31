585f4db85cdbc854e36991910fd11442461bb905
danielh4t <danielh4t@pm.me>
2022-08-31 15:58:03 +0200

    add edit fragment

---
+++ app/src/main/java/app/stacq/plan/EditFragment.kt
@@ -0,0 +1,31 @@
+package app.stacq.plan
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EditFragment()
+    }
+
+    private lateinit var viewModel: EditViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/app/stacq/plan/EditViewModel.kt
@@ -0,0 +1,7 @@
+package app.stacq.plan
+
+import androidx.lifecycle.ViewModel
+
+class EditViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
