998d5b002cdc997585819b5e32c6d884ff0b23fd
danielh4t <danielh4t@pm.me>
2022-08-31 14:10:21 +0200

    add completed button

--- app/src/main/java/app/stacq/plan/ui/task/TaskViewModel.kt
+++ app/src/main/java/app/stacq/plan/ui/task/TaskViewModel.kt
@@ -1,6 +1,9 @@
 package app.stacq.plan.ui.task
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import app.stacq.plan.data.model.TaskCategory
 import app.stacq.plan.data.source.repository.TasksRepository
 import kotlinx.coroutines.launch
@@ -12,6 +15,18 @@ class TaskViewModel(
 
     val task: LiveData<TaskCategory> = liveData {
         emitSource(tasksRepository.getTaskCategoryById(taskId))
+    }
+
+    fun complete() {
+        viewModelScope.launch {
+            task.value?.let {
+                tasksRepository.complete(
+                    it.id,
+                    !it.isCompleted,
+                    System.currentTimeMillis()
+                )
+            }
+        }
     }
 
     fun delete() {
