e0a69e71bded8a4f100a29bb3e5f3a861c35da4f
danielh4t <danielh4t@pm.me>
2022-09-02 23:27:40 +0200

    add task time binding adapter

--- app/src/main/java/app/stacq/plan/ui/tasks/TasksBindingUtils.kt
+++ app/src/main/java/app/stacq/plan/ui/tasks/TasksBindingUtils.kt
@@ -7,11 +7,24 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import app.stacq.plan.R
 import app.stacq.plan.data.model.TaskCategory
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 @BindingAdapter("taskCategory")
 fun TextView.getTaskCategoryTitle(taskCategory: TaskCategory) {
-        val category = taskCategory.categoryName
-        text = category
+    val category = taskCategory.categoryName
+    text = category
+}
+
+@BindingAdapter("taskTime")
+fun TextView.getTaskCategoryTitle(epoch: Long) {
+    val pattern = "EEEE, dd MMMM , yyyy HH:mm a"
+    val dateTime = DateTimeFormatter.ofPattern(pattern)
+        .withZone(ZoneOffset.UTC)
+        .format(Instant.ofEpochSecond(epoch))
+    text = dateTime
 }
 
 @BindingAdapter("taskCategoryColor")
