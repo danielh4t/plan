299d4fc09f56fdcb42467a9cf1fe5935df2bbeec
danielh4t <danielh4t@pm.me>
2022-09-14 12:09:21 +0300

    add firebase auth and safety check

--- app/src/main/java/app/stacq/plan/ui/MainActivity.kt
+++ app/src/main/java/app/stacq/plan/ui/MainActivity.kt
@@ -14,13 +14,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import app.stacq.plan.R
 import app.stacq.plan.databinding.ActivityMainBinding
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
+import app.stacq.plan.databinding.NavHeaderMainBinding
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.android.material.navigation.NavigationView
-import com.google.firebase.auth.AuthResult
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -28,18 +33,24 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHeaderMainBinding: NavHeaderMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var provider: OAuthProvider.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        provider = OAuthProvider.newBuilder("github.com")
-        firebaseAuth = Firebase.auth
-        showAuthenticatedUI(firebaseAuth.currentUser)
+
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        navHeaderMainBinding = NavHeaderMainBinding.inflate(layoutInflater)
+
+        firebaseAuth = Firebase.auth
 
         setContentView(binding.root)
         setSupportActionBar(binding.appBarMain.toolbar)
@@ -57,6 +68,33 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+    }
+
+
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            //val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(this, R.string.sign_in_success, Toast.LENGTH_SHORT).show()
+        } else {
+            // Sign in failed
+            // If response is null the user canceled the sign-in flow using the back button.
+            // Otherwise check response.getError().getErrorCode() and handle the error.
+            if (response == null) {
+                Toast.makeText(this, R.string.sign_in_dismiss, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, R.string.sign_in_failure, Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -84,44 +122,32 @@ class MainActivity : AppCompatActivity() {
         return navHostFragment.navController
     }
 
-    private fun handleAuthentication() {
-        val pendingResultTask: Task<AuthResult>? = firebaseAuth.pendingAuthResult
-        if (pendingResultTask != null) {
-            pendingResultTask
-                .addOnSuccessListener(
-                    OnSuccessListener {
-                        // User is signed in.
-                        Toast.makeText(this, R.string.sign_in_success, Toast.LENGTH_SHORT).show()
-                        // authResult.getCredential().getAccessToken().
-                    })
-                .addOnFailureListener {
-                    // Handle failure.
-                    Toast.makeText(this, R.string.sign_in_failure, Toast.LENGTH_SHORT).show()
-                }
-        } else {
-            // There's no pending result so you need to start the sign-in flow.
-            firebaseAuth
-                .startActivityForSignInWithProvider( /* activity= */this, provider.build())
-                .addOnSuccessListener {
-                    // User is signed in.
-                    // IdP data available in
-                    // authResult.getAdditionalUserInfo().getProfile().
-                    // The OAuth access token can also be retrieved:
-                    // authResult.getCredential().getAccessToken().
-                    Toast.makeText(this, R.string.sign_in_success, Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    // Handle failure.
-                    Toast.makeText(this, R.string.sign_in_failure, Toast.LENGTH_SHORT).show()
-                }
-        }
-    }
 
     private fun showAuthenticatedUI(user: FirebaseUser?) {
         if (user != null) {
-            Toast.makeText(this, user.displayName, Toast.LENGTH_SHORT).show()
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    Toast.makeText(this, R.string.sign_out_success, Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(this, R.string.sign_out_failure, Toast.LENGTH_SHORT).show()
+                }
+
         } else {
-            handleAuthentication()
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+            )
+            // Create and launch sign-in intent
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.mipmap.ic_launcher)
+                .setTheme(R.style.Theme_Plan)
+                .build()
+            signInLauncher.launch(signInIntent)
+
         }
     }
+
+
 }
