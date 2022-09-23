0ea3900337b41bb4b594edc360d96a4ef2c295a6
danielh4t <danielh4t@pm.me>
2022-09-23 22:47:47 +0200

    update task timer to seventy minutes

--- app/src/main/java/app/stacq/plan/ui/timer/TimerConstants.kt
+++ app/src/main/java/app/stacq/plan/ui/timer/TimerConstants.kt
@@ -1,6 +1,9 @@
 package app.stacq.plan.ui.timer
 
 object TimerConstants {
-    const val TIMER_TIME_IN_SECONDS: Long = 5400L
+    const val TIMER_TIME_IN_SECONDS: Long = 4200L
     const val TIMER_TICK_IN_SECONDS: Long = 60L
 }
+
+
+98
