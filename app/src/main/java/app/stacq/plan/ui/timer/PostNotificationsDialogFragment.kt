513f3d237606f36059f6093753a52a9999c7d28d
danielh4t <danielh4t@pm.me>
2022-09-29 23:31:25 +0200

    add request in post notifications dialog

--- app/src/main/java/app/stacq/plan/ui/timer/PostNotificationsDialogFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/timer/PostNotificationsDialogFragment.kt
@@ -1,18 +1,47 @@
 package app.stacq.plan.ui.timer
 
+import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import app.stacq.plan.R
 
 class PostNotificationsDialogFragment : DialogFragment() {
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                Log.d(TAG, "Allowed")
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Log.d(TAG, "Denied")
+            }
+        }
+
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         AlertDialog.Builder(requireContext())
             .setMessage(getString(R.string.post_notifications))
-            .setPositiveButton(getString(R.string.allow)) { _, _ -> }
-            .setNegativeButton(getString(R.string.deny))
+            .setPositiveButton(getString(R.string.allow)) {_,_ ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            .setNegativeButton(getString(R.string.deny)) {_,_ ->
+                Log.d(TAG, "Denied")
+            }
             .create()
 
     companion object {
