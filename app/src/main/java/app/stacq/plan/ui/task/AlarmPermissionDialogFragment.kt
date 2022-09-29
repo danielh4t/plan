3b8adc1503fab18b8072598cae5586eabd1c6775
danielh4t <danielh4t@pm.me>
2022-09-29 22:20:10 +0200

    add create dialog on alarm permission dialog

--- app/src/main/java/app/stacq/plan/ui/task/AlarmPermissionDialogFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/task/AlarmPermissionDialogFragment.kt
@@ -1,4 +1,17 @@
 package app.stacq.plan.ui.task
 
-class AlarmPermissionDialogFragment {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import app.stacq.plan.R
+
+class AlarmPermissionDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage(getString(R.string.alarm_permission))
+            .setPositiveButton(getString(R.string.ok)) { _, _ -> }
+            .create()
+
 }
