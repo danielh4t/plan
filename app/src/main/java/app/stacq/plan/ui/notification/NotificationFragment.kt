efde02037a62edd14c5e387c4ae55673c28a3f63
danielh4t <danielh4t@pm.me>
2022-10-01 19:47:24 +0200

    refactor notification fragment

---
+++ app/src/main/java/app/stacq/plan/ui/notification/NotificationFragment.kt
@@ -0,0 +1,27 @@
+package app.stacq.plan.ui.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.stacq.plan.databinding.FragmentNotificationBinding
+
+
+class NotificationFragment : Fragment() {
+
+    private var _binding: FragmentNotificationBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
