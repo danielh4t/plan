43d5ab94bb39862282f23518dc790227634db95d
danielh4t <danielh4t@pm.me>
2022-10-01 20:55:57 +0200

    remove preloaded fonts

--- app/src/main/java/app/stacq/plan/ui/notification/NotificationFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/notification/NotificationFragment.kt
@@ -1,11 +1,22 @@
 package app.stacq.plan.ui.notification
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import app.stacq.plan.R
+import app.stacq.plan.data.model.TaskCategory
 import app.stacq.plan.databinding.FragmentNotificationBinding
+import app.stacq.plan.ui.timer.PostNotificationsDialogFragment
+import com.google.android.material.snackbar.Snackbar
 
 
 class NotificationFragment : Fragment() {
@@ -14,6 +25,8 @@ class NotificationFragment : Fragment() {
     private val binding get() = _binding!!
 
 
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,7 +34,44 @@ class NotificationFragment : Fragment() {
 
         _binding = FragmentNotificationBinding.inflate(inflater, container, false)
 
+        val args = NotificationFragmentArgs.fromBundle(requireArguments())
+        val task: TaskCategory = args.taskCategory
+
+        val viewModel: NotificationViewModel by viewModels()
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted.
+                    Log.d(PostNotificationsDialogFragment.TAG, "Permission granted")
+                    viewModel.logPermission(true)
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied.
+                    Log.d(PostNotificationsDialogFragment.TAG, "Permission denied")
+                    Snackbar.make(
+                        binding.notifyButton,
+                        R.string.no_notification,
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    viewModel.logPermission(false)
+
+                }
+                val action = NotificationFragmentDirections.actionNavNotificationToNavTimer(task)
+                this.findNavController().navigate(action)
+            }
+
+        binding.notifyButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+
         return binding.root
     }
+
 
 }
