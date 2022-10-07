93436a54b3406db6e42c4508b2f7e597be570cf5
danielh4t <danielh4t@pm.me>
2022-10-07 14:14:00 +0200

    remove plan api service

---
+++ app/src/main/java/app/stacq/plan/data/source/remote/task/TasksDataStore.kt
@@ -0,0 +1,11 @@
+package app.stacq.plan.data.source.remote.task
+
+import app.stacq.plan.data.model.Task
+
+class TasksDataStore() {
+
+    suspend fun createTask(task: Task) {
+
+    }
+
+}
--- app/src/main/java/app/stacq/plan/data/source/remote/task/TasksRemoteDataSource.kt
+++ app/src/main/java/app/stacq/plan/data/source/remote/task/TasksRemoteDataSource.kt
@@ -8,36 +8,12 @@ import kotlinx.coroutines.withContext
 
 
 class TasksRemoteDataSource(
-    private val planApiService: PlanApiService,
+    private val tasksDataStore: TasksDataStore,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    suspend fun getTasks(): List<Task> = withContext(ioDispatcher) {
-        planApiService.getTasks()
-    }
-
     suspend fun createTask(task: Task) = withContext(ioDispatcher) {
-        planApiService.createTask(task)
-    }
-
-    suspend fun readTaskById(id: String): Task = withContext(ioDispatcher) {
-        planApiService.readTaskById(id)
-    }
-
-    suspend fun updateTask(task: Task) = withContext(ioDispatcher) {
-        planApiService.updateTask(task)
-    }
-
-    suspend fun deleteTaskById(id: String) = withContext(ioDispatcher) {
-        planApiService.deleteTaskById(id)
-    }
-
-    suspend fun getTasksCategory(): List<TaskCategory> = withContext(ioDispatcher) {
-        planApiService.getTasksCategory()
-    }
-
-    suspend fun readTaskCategoryById(id: String): List<TaskCategory> = withContext(ioDispatcher) {
-        planApiService.readTaskCategoryById(id)
+        tasksDataStore.createTask(task)
     }
 
 }
