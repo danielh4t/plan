2a10828f3bf3e7b932213239fe39fda4dbe73b0a
danielh4t <danielh4t@pm.me>
2022-11-01 15:11:28 +0100

    add color palette

--- app/src/main/java/app/stacq/plan/ui/category/CategoryViewModel.kt
+++ app/src/main/java/app/stacq/plan/ui/category/CategoryViewModel.kt
@@ -33,12 +33,30 @@ class CategoryViewModel(private val categoryRepository: CategoryRepository) : Vi
 
     private fun defaultColors(color: String): String {
         return when (color) {
-            "Code" -> "#ffff7f50"
-            "Hack" -> "#ff2ed573"
-            "Life" -> "#fffdcd21"
-            "Work" -> "#ff1e90ff"
-            else ->  String.format("#ff%06x", Random.nextInt(0xffffff + 1))
+            "Code" -> "#FF7F50"
+            "Hack" -> "#2ED573"
+            "Life" -> "#FDCD21"
+            "Work" -> "#1E90FF"
+            else -> colorPalette()
         }
+    }
+
+    private fun colorPalette(): String {
+        val palette = listOf(
+            "FF74B1",
+            "FFB200",
+            "B2A4FF",
+            "FF00E4",
+            "FF4848",
+            "00EAD3",
+            "FC5404",
+            "F637EC",
+            "4D77FF",
+            "93FFD8"
+        )
+
+        return palette.random()
+
     }
 
 }
