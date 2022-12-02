bcf6d5d2e0d5896a0a20f62dea59fed6b3942ea5
danielh4t <danielh4t@pm.me>
2022-12-02 11:09:53 +0100

    add timer finished on task timer

--- app/src/main/java/app/stacq/plan/ui/task/TaskBindingUtils.kt
+++ app/src/main/java/app/stacq/plan/ui/task/TaskBindingUtils.kt
@@ -2,6 +2,7 @@ package app.stacq.plan.ui.task
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import app.stacq.plan.R
 import app.stacq.plan.ui.timer.TimerConstants.TIMER_TIME_IN_SECONDS
 import app.stacq.plan.util.millisInFuture
 import java.time.Instant
@@ -21,10 +22,17 @@ fun TextView.setTaskTime(epoch: Long) {
 @BindingAdapter("taskTimer")
 fun TextView.setTaskTimer(timerFinishAt: Long) {
     val minutes = if (timerFinishAt > 0) {
+        // timer started
         millisInFuture(timerFinishAt) / 60 / 1000L
     } else {
         // timer not started
         TIMER_TIME_IN_SECONDS / 60
     }
-    text = String.format("%d minutes", minutes)
+
+    text = if (minutes == 0L) {
+        // timer finished
+        setText(R.string.timer_complete).toString()
+    }else {
+        String.format("%d minutes", minutes)
+    }
 }
