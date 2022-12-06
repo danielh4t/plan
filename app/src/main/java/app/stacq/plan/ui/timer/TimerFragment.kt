b6e9a1c8c2ee2dfe81313548b7771350ca04b4ef
danielh4t <danielh4t@pm.me>
2022-12-06 08:08:15 +0100

    refactor alarm and notification from timer fragment to utils

--- app/src/main/java/app/stacq/plan/ui/timer/TimerFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/timer/TimerFragment.kt
@@ -1,29 +1,21 @@
 package app.stacq.plan.ui.timer
 
-import android.app.AlarmManager
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.graphics.drawable.Animatable
-import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import app.stacq.plan.R
 import app.stacq.plan.data.source.local.PlanDatabase
 import app.stacq.plan.data.source.local.task.TaskLocalDataSource
 import app.stacq.plan.data.source.model.Task
 import app.stacq.plan.data.source.remote.task.TaskRemoteDataSource
 import app.stacq.plan.data.source.repository.TaskRepository
 import app.stacq.plan.databinding.FragmentTimerBinding
-import app.stacq.plan.util.createNotificationChannel
-import app.stacq.plan.util.millisInFuture
+import app.stacq.plan.util.cancelAlarm
+import app.stacq.plan.util.createTimerChannel
+import app.stacq.plan.util.setAlarm
 
 
 class TimerFragment : Fragment() {
@@ -34,8 +26,6 @@ class TimerFragment : Fragment() {
     private lateinit var viewModelFactory: TimerViewModelFactory
     private lateinit var viewModel: TimerViewModel
 
-    private var alarmManager: AlarmManager? = null
-    private var notificationPendingIntent: PendingIntent? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -70,7 +60,12 @@ class TimerFragment : Fragment() {
             if (it && notify) {
                 setAlarm(application, task.timerFinishAt, task.name)
             } else {
-                cancelAlarm()
+                cancelAlarm(
+                    application,
+                    task.timerFinishAt,
+                    task.name
+                )
+
             }
         }
 
@@ -81,63 +76,6 @@ class TimerFragment : Fragment() {
         }
 
         createTimerChannel(application)
-    }
-
-    private fun createTimerChannel(applicationContext: Context) {
-        val channelId = applicationContext.getString(R.string.timer_channel_id)
-        val channelName = applicationContext.getString(R.string.timer_channel_name)
-        val description = applicationContext.getString(R.string.timer_channel_description)
-        val notificationManager: NotificationManager =
-            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        notificationManager.createNotificationChannel(channelId, channelName, description)
-    }
-
-    private fun setAlarm(applicationContext: Context, finishAt: Long, title: String) {
-
-        val notificationIntent: Intent = Intent(applicationContext, TimerReceiver::class.java)
-            .putExtra(TimerConstants.TIMER_RECEIVER_ID_KEY, finishAt)
-            .putExtra(TimerConstants.TIMER_RECEIVER_TEXT_KEY, title)
-
-
-        notificationPendingIntent =
-            PendingIntent.getBroadcast(
-                applicationContext,
-                finishAt.toInt(),
-                notificationIntent,
-                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-            )
-
-        val triggerTime = SystemClock.elapsedRealtime() + millisInFuture(finishAt)
-        alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (alarmManager?.canScheduleExactAlarms() == true) {
-                alarmManager?.setExactAndAllowWhileIdle(
-                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    triggerTime,
-                    notificationPendingIntent
-                )
-            } else {
-                val intent = Intent().apply {
-                    action = Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
-                }
-                startActivity(intent)
-            }
-
-        } else {
-            alarmManager?.setExactAndAllowWhileIdle(
-                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                triggerTime,
-                notificationPendingIntent
-            )
-        }
-    }
-
-    private fun cancelAlarm() {
-        if (notificationPendingIntent != null) {
-            alarmManager?.cancel(notificationPendingIntent)
-        }
     }
 
 
--- app/src/main/java/app/stacq/plan/util/Util.kt
+++ app/src/main/java/app/stacq/plan/util/Util.kt
@@ -1,6 +1,17 @@
 package app.stacq.plan.util
 
 
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.SystemClock
+import android.provider.Settings
+import app.stacq.plan.R
+import app.stacq.plan.ui.timer.TimerConstants
+import app.stacq.plan.ui.timer.TimerReceiver
 import java.time.Instant
 import java.time.YearMonth
 import java.util.*
@@ -63,14 +74,70 @@ fun startOfDay(day: Int): Long {
     return calendar.timeInMillis / 1000L
 }
 
-fun endOfDay(day: Int): Long {
-    val calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
-    calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR))
-    calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH))
-    calendar.set(Calendar.DAY_OF_MONTH, day + 1)
-    calendar.set(Calendar.HOUR_OF_DAY, 23)
-    calendar.set(Calendar.MINUTE, 59)
-    calendar.set(Calendar.SECOND, 59)
-    calendar.set(Calendar.MILLISECOND, 999)
-    return calendar.timeInMillis / 1000L
+fun createTimerChannel(applicationContext: Context) {
+    val channelId = applicationContext.getString(R.string.timer_channel_id)
+    val channelName = applicationContext.getString(R.string.timer_channel_name)
+    val description = applicationContext.getString(R.string.timer_channel_description)
+    val notificationManager: NotificationManager =
+        applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.createNotificationChannel(channelId, channelName, description)
+}
+
+fun setAlarm(applicationContext: Context, finishAt: Long, name: String) {
+    val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    val notificationIntent: Intent = Intent(applicationContext, TimerReceiver::class.java)
+        .putExtra(TimerConstants.TIMER_RECEIVER_ID_KEY, finishAt)
+        .putExtra(TimerConstants.TIMER_RECEIVER_TEXT_KEY, name)
+
+    val notificationPendingIntent =
+        createPendingIntent(applicationContext, finishAt, notificationIntent)
+
+    val triggerTime = SystemClock.elapsedRealtime() + millisInFuture(finishAt)
+
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (alarmManager.canScheduleExactAlarms()) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                triggerTime,
+                notificationPendingIntent
+            )
+        } else {
+            val intent = Intent().apply {
+                action = Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+            }
+
+            applicationContext.startActivity(intent)
+        }
+
+    } else {
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            triggerTime,
+            notificationPendingIntent
+        )
+    }
+}
+
+private fun createPendingIntent(
+    applicationContext: Context,
+    finishAt: Long,
+    notificationIntent: Intent
+) = PendingIntent.getBroadcast(
+    applicationContext,
+    finishAt.toInt(),
+    notificationIntent,
+    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+)
+
+
+fun cancelAlarm(applicationContext: Context, finishAt: Long, name: String) {
+    val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    val notificationIntent: Intent = Intent(applicationContext, TimerReceiver::class.java)
+        .putExtra(TimerConstants.TIMER_RECEIVER_ID_KEY, finishAt)
+        .putExtra(TimerConstants.TIMER_RECEIVER_TEXT_KEY, name)
+
+    val pendingIntent = createPendingIntent(applicationContext, finishAt, notificationIntent)
+
+    alarmManager.cancel(pendingIntent)
 }
