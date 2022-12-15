c6fd90a3ee7c462421e7561eed78739b3b946491
danielh4t <danielh4t@pm.me>
2022-12-15 21:58:19 +0100

    add goal entity to model goal on local database

---
+++ app/src/main/java/app/stacq/plan/data/source/local/goal/GoalEntity.kt
@@ -0,0 +1,34 @@
+package app.stacq.plan.data.source.local.goal
+
+import androidx.annotation.Keep
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.*
+
+
+@Keep
+@Entity(tableName = "goal")
+data class GoalEntity(
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String = UUID.randomUUID().toString(),
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long = Instant.now().epochSecond,
+
+    var name: String,
+
+    @ColumnInfo(name = "category_id")
+    var categoryId: String,
+
+    var achieved: Boolean = false,
+
+    @ColumnInfo(name = "achieved_at")
+    var achievedAt: Long = 0,
+
+    @ColumnInfo(name = "priority")
+    var priority: Int = 0
+)
