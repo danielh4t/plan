5d5e6f99175a8d096423fb16134a0046b11106cf
danielh4t <danielh4t@pm.me>
2022-12-15 22:32:12 +0100

    add achieved by to goal entity and remove priority

--- app/src/main/java/app/stacq/plan/data/source/local/goal/GoalEntity.kt
+++ app/src/main/java/app/stacq/plan/data/source/local/goal/GoalEntity.kt
@@ -26,9 +26,9 @@ data class GoalEntity(
 
     var achieved: Boolean = false,
 
+    @ColumnInfo(name = "achieved_by")
+    var achievedBy: Long,
+
     @ColumnInfo(name = "achieved_at")
     var achievedAt: Long = 0,
-
-    @ColumnInfo(name = "priority")
-    var priority: Int = 0
 )
