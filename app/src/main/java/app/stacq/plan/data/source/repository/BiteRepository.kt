9b11799d3620d2111e4af39fdc32e10822857e13
danielh4t <danielh4t@pm.me>
2023-01-11 08:30:01 +0100

    refactor get bites to remove emit source

--- app/src/main/java/app/stacq/plan/data/source/repository/BiteRepository.kt
+++ app/src/main/java/app/stacq/plan/data/source/repository/BiteRepository.kt
@@ -1,14 +1,14 @@
 package app.stacq.plan.data.source.repository
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.map
+import androidx.lifecycle.Transformations
 import app.stacq.plan.data.source.local.bite.BiteEntity
 import app.stacq.plan.data.source.local.bite.BiteLocalDataSource
+import app.stacq.plan.data.source.remote.bite.BiteRemoteDataSource
 import app.stacq.plan.domain.Bite
 import app.stacq.plan.domain.asBite
 import app.stacq.plan.domain.asDocument
 import app.stacq.plan.domain.asEntity
-import app.stacq.plan.data.source.remote.bite.BiteRemoteDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -19,11 +19,6 @@ class BiteRepository(
     private val remoteDataSource: BiteRemoteDataSource,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
-
-    suspend fun getBites(taskId: String): LiveData<List<Bite>> = withContext(ioDispatcher) {
-        localDataSource.getBites(taskId)
-            .map { biteEntities: List<BiteEntity> -> biteEntities.map { biteEntity -> biteEntity.asBite() } }
-    }
 
     suspend fun create(bite: Bite) = withContext(ioDispatcher) {
         localDataSource.create(bite.asEntity())
@@ -47,4 +42,8 @@ class BiteRepository(
         remoteDataSource.update(biteEntity.asDocument())
     }
 
+    fun getBites(taskId: String): LiveData<List<Bite>> =
+        Transformations.map(localDataSource.getBites(taskId)) {
+            it?.map { biteEntity: BiteEntity -> biteEntity.asBite() }
+        }
 }
