ff043aa414d372f517acf84cd3d19fc4f83dbc4d
danielh4t <danielh4t@pm.me>
2023-02-03 23:14:34 +0100

    add dynamic colors to application

--- app/src/main/java/app/stacq/plan/PlanApplication.kt
+++ app/src/main/java/app/stacq/plan/PlanApplication.kt
@@ -1,7 +1,11 @@
 package app.stacq.plan
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 
 class PlanApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
