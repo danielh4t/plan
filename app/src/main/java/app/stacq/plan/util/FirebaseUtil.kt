8e6210209a6153161326bf70ffb29c563400e4f5
danielh4t <danielh4t@pm.me>
2023-02-09 01:01:31 +0100

    add firebase util

--- app/src/main/java/app/stacq/plan/util/FirebaseUtil.kt
+++ app/src/main/java/app/stacq/plan/util/FirebaseUtil.kt
@@ -1,22 +1,9 @@
 package app.stacq.plan.util
 
-import app.stacq.plan.BuildConfig
 import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 
 
 fun FirebaseAppCheck.installCheckProviderFactory() {
-
-    if (BuildConfig.DEBUG) {
-        // do something for a debug build
-        installAppCheckProviderFactory(
-            DebugAppCheckProviderFactory.getInstance()
-        )
-    } else {
-        installAppCheckProviderFactory(
-            PlayIntegrityAppCheckProviderFactory.getInstance()
-        );
-    }
-
+    installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance())
 }
