fe8fba7e83102075552743761d3917912cd9de49
danielh4t <danielh4t@gmail.com>
2023-03-14 12:06:58 +0100

    add bite modify menu listener to call delete method to delete bite

--- app/src/main/java/app/stacq/plan/ui/biteModify/BiteModifyViewModel.kt
+++ app/src/main/java/app/stacq/plan/ui/biteModify/BiteModifyViewModel.kt
@@ -43,4 +43,13 @@ class BiteModifyViewModel(
             }
         }
     }
+
+    fun delete() {
+        val bite: Bite? = bite.value
+        viewModelScope.launch {
+            bite?.let {
+                biteRepository.delete(it)
+            }
+        }
+    }
 }
