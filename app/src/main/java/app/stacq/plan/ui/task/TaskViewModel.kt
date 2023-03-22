c5a0059d12a069985c50a37fff066379e3dad2a5
danielh4t <danielh4t@gmail.com>
2023-03-22 13:26:51 +0100

    update clone task to filter and move bites

--- app/src/main/java/app/stacq/plan/ui/task/TaskViewModel.kt
+++ app/src/main/java/app/stacq/plan/ui/task/TaskViewModel.kt
@@ -5,19 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.stacq.plan.data.repository.bite.BiteRepository
 import app.stacq.plan.data.repository.task.TaskRepository
-import app.stacq.plan.data.source.local.bite.BiteEntity
 import app.stacq.plan.data.source.local.task.TaskEntity
 import app.stacq.plan.domain.Bite
 import app.stacq.plan.domain.Task
-import app.stacq.plan.domain.asBite
 import app.stacq.plan.domain.asTask
 import app.stacq.plan.util.constants.AnalyticsConstants
+import app.stacq.plan.util.time.TimeUtil
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
-import java.time.Instant
 
 
 class TaskViewModel(
@@ -37,85 +35,96 @@ class TaskViewModel(
             param("notifications", if (isGranted) "true" else "false")
         }
     }
+
     fun clone() {
         val name = task.value?.name
         val categoryId = task.value?.categoryId
         val goalId = task.value?.goalId
+        var clonedTask: Task? = null
+        if (name != null && categoryId != null) {
+            val taskEntity = TaskEntity(name = name, categoryId = categoryId, goalId = goalId)
+            clonedTask = taskEntity.asTask()
+        }
+
         val bites = bites.value
         viewModelScope.launch {
-            if (name != null && categoryId != null && bites != null) {
-                val taskEntity = TaskEntity(name = name, categoryId = categoryId, goalId = goalId)
-                val task = taskEntity.asTask()
-                taskRepository.create(task)
-                // clone incomplete bites
-                bites.filter { bite -> !bite.completed }.forEach {
-                    val biteEntity = BiteEntity(name = it.name, taskId = task.id, categoryId = categoryId)
-                    bitesRepository.create(biteEntity.asBite())
-                }
-            }
-        }
-    }
-
-    fun delete() {
-        val task: Task? = task.value
-        val bites: List<Bite>? = bites.value
-        task?.let {
-            viewModelScope.launch {
-                taskRepository.delete(it)
-                bites?.let {
-                    it.forEach { bite ->
-                        bitesRepository.delete(bite)
+            if (clonedTask != null) {
+                // clone task
+                taskRepository.create(clonedTask)
+                if (bites != null) {
+                    // move incomplete bites
+                    val incompleteBites = bites.filter { !it.completed }.map {
+                        it.copy(taskId = clonedTask.id)
+                    }
+                    incompleteBites.map {
+                        bitesRepository.update(it)
                     }
                 }
             }
         }
     }
 
-    fun undoDelete() {
-        val task: Task? = task.value
-        val bites: List<Bite>? = bites.value
-        task?.let {
-            viewModelScope.launch {
-                taskRepository.create(it)
-                bites?.let {
-                    it.forEach { bite ->
-                        bitesRepository.create(bite)
+
+        fun delete() {
+            val task: Task? = task.value
+            val bites: List<Bite>? = bites.value
+            task?.let {
+                viewModelScope.launch {
+                    taskRepository.delete(it)
+                    bites?.let {
+                        it.forEach { bite ->
+                            bitesRepository.delete(bite)
+                        }
                     }
                 }
             }
         }
-    }
 
-    fun updatePriority(priority: Float) {
-        val task: Task? = task.value
-        task?.let {
-            it.priority = priority.toInt()
-            viewModelScope.launch {
-                taskRepository.updatePriority(it)
+        fun undoDelete() {
+            val task: Task? = task.value
+            val bites: List<Bite>? = bites.value
+            task?.let {
+                viewModelScope.launch {
+                    taskRepository.create(it)
+                    bites?.let {
+                        it.forEach { bite ->
+                            bitesRepository.create(bite)
+                        }
+                    }
+                }
             }
         }
-    }
 
-    fun completeBite(bite: Bite) {
-        bite.completed = !bite.completed
-        bite.completedAt = Instant.now().epochSecond
-        viewModelScope.launch {
-            bitesRepository.update(bite)
+        fun updatePriority(priority: Float) {
+            val task: Task? = task.value
+            task?.let {
+                it.priority = priority.toInt()
+                viewModelScope.launch {
+                    taskRepository.updatePriority(it)
+                }
+            }
+        }
+
+        fun completeBite(bite: Bite) {
+            bite.completed = !bite.completed
+            bite.completedAt = TimeUtil().nowInSeconds()
+            viewModelScope.launch {
+                bitesRepository.update(bite)
+            }
+        }
+
+        fun deleteBite(bite: Bite) {
+            viewModelScope.launch {
+                bitesRepository.delete(bite)
+            }
+        }
+
+        fun hasAlarm(): Boolean {
+            val task: Task? = task.value
+            task?.let {
+                if (it.timerAlarm && it.timerFinishAt > TimeUtil().nowInSeconds())
+                    return true
+            }
+            return false
         }
     }
-
-    fun deleteBite(bite: Bite) {
-        viewModelScope.launch {
-            bitesRepository.delete(bite)
-        }
-    }
-
-    fun hasAlarm(): Boolean {
-        val task: Task? = task.value
-        task?.let {
-            if (it.timerAlarm && it.timerFinishAt > Instant.now().epochSecond)
-                return true
-        }
-        return false
-    }
-}
