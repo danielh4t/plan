7b53a5920bf3306bc1d0b830426c31cf71c03563
danielh4t <danielh4t@gmail.com>
2023-03-29 14:33:40 +0200

    refactor remove add default category database and fallback

--- app/src/main/java/app/stacq/plan/data/source/local/PlanDatabase.kt
+++ app/src/main/java/app/stacq/plan/data/source/local/PlanDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import app.stacq.plan.data.source.local.bite.BiteDao
 import app.stacq.plan.data.source.local.bite.BiteEntity
 import app.stacq.plan.data.source.local.category.CategoryDao
@@ -13,9 +12,6 @@ import app.stacq.plan.data.source.local.goal.GoalDao
 import app.stacq.plan.data.source.local.goal.GoalEntity
 import app.stacq.plan.data.source.local.task.TaskDao
 import app.stacq.plan.data.source.local.task.TaskEntity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 @Database(
@@ -45,20 +41,7 @@ abstract class PlanDatabase : RoomDatabase() {
                         context.applicationContext,
                         PlanDatabase::class.java,
                         PLAN_DATABASE
-                    )
-                        .addCallback(object : Callback() {
-                            override fun onCreate(db: SupportSQLiteDatabase) {
-                                super.onCreate(db)
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    val categoryEntity =
-                                        CategoryEntity(name = "Task", color = "#3ddb85")
-                                    getDatabase(context).categoryDao().insert(categoryEntity)
-                                }
-                            }
-                        })
-                        .fallbackToDestructiveMigration()
-                        .build()
-
+                    ).build()
                     INSTANCE = instance
                 }
                 return instance
