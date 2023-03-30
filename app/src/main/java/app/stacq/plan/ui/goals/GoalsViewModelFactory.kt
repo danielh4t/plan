dcb01e649609b671ccadb8d9af754cce64106a40
danielh4t <danielh4t@gmail.com>
2023-03-31 01:14:38 +0200

    add category repository to goals fragment and view model

--- app/src/main/java/app/stacq/plan/ui/goals/GoalsViewModelFactory.kt
+++ app/src/main/java/app/stacq/plan/ui/goals/GoalsViewModelFactory.kt
@@ -4,16 +4,20 @@ package app.stacq.plan.ui.goals
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import app.stacq.plan.data.repository.category.CategoryRepository
 import app.stacq.plan.data.repository.goal.GoalRepository
 
 
-class GoalsViewModelFactory(private val goalRepository: GoalRepository) : ViewModelProvider.Factory {
+class GoalsViewModelFactory(
+    private val goalRepository: GoalRepository,
+    private val categoryRepository: CategoryRepository
+) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         with(modelClass) {
             when {
                 isAssignableFrom(GoalsViewModel::class.java) ->
-                    return GoalsViewModel(goalRepository) as T
+                    return GoalsViewModel(goalRepository, categoryRepository) as T
                 else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
             }
         }
