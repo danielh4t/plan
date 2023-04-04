47c83fa554e95e07f97612fe86731a90b73e159f
danielh4t <danielh4t@gmail.com>
2023-04-04 16:36:42 +0200

    fix post notifications permission request logic and strings

--- app/src/main/java/app/stacq/plan/domain/receivers/TimerCompleteReceiver.kt
+++ app/src/main/java/app/stacq/plan/domain/receivers/TimerCompleteReceiver.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationManagerCompat
 import app.stacq.plan.util.constants.TimerConstants
-
 import app.stacq.plan.util.sendNotification
 
 
