702ffd6a3620ba0e42e3969bf704453dd92a9b2a
danielh4t <danielh4t@gmail.com>
2023-04-13 18:19:53 +0200

    add account image to goals and categories

--- app/src/main/java/app/stacq/plan/ui/goals/GoalsFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/goals/GoalsFragment.kt
@@ -1,9 +1,15 @@
 package app.stacq.plan.ui.goals
 
+import android.app.Activity
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -17,12 +23,21 @@ import app.stacq.plan.data.source.local.goal.GoalLocalDataSourceImpl
 import app.stacq.plan.data.source.remote.category.CategoryRemoteDataSourceImpl
 import app.stacq.plan.data.source.remote.goal.GoalRemoteDataSourceImpl
 import app.stacq.plan.databinding.FragmentGoalsBinding
+import app.stacq.plan.util.handleSignInWithFirebase
+import app.stacq.plan.util.launchSignIn
 import app.stacq.plan.util.ui.MarginItemDecoration
+import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.android.gms.auth.api.identity.SignInClient
+import coil.load
+import coil.size.ViewSizeResolver
+import coil.transform.CircleCropTransformation
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class GoalsFragment : Fragment() {
@@ -33,6 +48,9 @@ class GoalsFragment : Fragment() {
     private lateinit var viewModelFactory: GoalsViewModelFactory
     private lateinit var viewModel: GoalsViewModel
 
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+    private lateinit var oneTapClient: SignInClient
+    private var showOneTapUI: Boolean = true
     private var hasCategories: Boolean = false
 
     override fun onCreateView(
@@ -67,7 +85,48 @@ class GoalsFragment : Fragment() {
         binding.goalsAppBarLayout.statusBarForeground =
             MaterialShapeDrawable.createWithElevationOverlay(context)
 
+        binding.goalsAccountImageView.setOnClickListener {
+            val message = if (Firebase.auth.currentUser == null) {
+                resources.getString(R.string.sign_in_sync)
+            } else {
+                resources.getString(R.string.sign_out_sync)
+            }
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(resources.getString(R.string.backup_sync))
+                .setMessage(message)
+                .setNegativeButton(resources.getString(R.string.no_thanks)) { _, _ ->
+                    // Respond to negative button press
+                    Toast.makeText(requireContext(), R.string.no_backup, Toast.LENGTH_SHORT).show()
+                }
+                .setPositiveButton(resources.getString(R.string.yes_please)) { _, _ ->
+                    // Respond to positive button press
+                    handleAuthentication()
+                }
+                .show()
+        }
+
         val navController = findNavController()
+
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        authStateListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if (user != null) {
+                // signed in
+                if (user.photoUrl !== null) {
+                    binding.goalsAccountImageView.load(user.photoUrl) {
+                        crossfade(true)
+                        size(ViewSizeResolver(binding.goalsAccountImageView))
+                        transformations(CircleCropTransformation())
+                    }
+                }
+            } else {
+                // signed out
+                binding.goalsAccountImageView.setImageResource(R.drawable.ic_account_circle)
+            }
+        }
+
+        Firebase.auth.addAuthStateListener(authStateListener)
 
         val goalNavigateListener = GoalNavigateListener {
             val action = GoalsFragmentDirections.actionNavGoalsToNavGoal(it)
@@ -109,7 +168,6 @@ class GoalsFragment : Fragment() {
             }
         }
 
-
         viewModel.categories.observe(viewLifecycleOwner) {
             it?.let {
                 hasCategories = it.isNotEmpty()
@@ -120,5 +178,43 @@ class GoalsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        Firebase.auth.removeAuthStateListener(authStateListener)
     }
+
+    private fun handleAuthentication() {
+        // if user isn't signed in and hasn't already declined to use One Tap sign-in
+        if (showOneTapUI && Firebase.auth.currentUser == null) {
+            val clientId = getString(R.string.default_web_client_id)
+            oneTapClient.launchSignIn(clientId)
+                .addOnSuccessListener { result ->
+                    try {
+                        val intentSenderRequest =
+                            IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                        signInLauncher.launch(intentSenderRequest)
+
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e("Plan", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d("Plan", "Failure: ${e.localizedMessage}")
+                }
+            // don't
+            showOneTapUI = false
+        } else {
+            // sign out
+            Firebase.auth.signOut()
+            oneTapClient.signOut()
+            showOneTapUI = true
+        }
+    }
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                oneTapClient.handleSignInWithFirebase(it.data)
+            }
+        }
 }
--- app/src/main/java/app/stacq/plan/ui/tasks/TasksFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/tasks/TasksFragment.kt
@@ -38,11 +38,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import coil.load
 import coil.size.ViewSizeResolver
 import coil.transform.CircleCropTransformation
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import java.util.concurrent.TimeUnit
 
 
