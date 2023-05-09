80e17ed394491f18f8b1aa354202a1e2532953bd
danielh4t <danielh4t@gmail.com>
2023-05-10 00:50:25 +0200

    add icon button and image view to upload and modify task image

--- app/src/main/java/app/stacq/plan/ui/taskModify/TaskModifyBindingUtils.kt
+++ app/src/main/java/app/stacq/plan/ui/taskModify/TaskModifyBindingUtils.kt
@@ -1,7 +1,11 @@
 package app.stacq.plan.ui.taskModify
 
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import app.stacq.plan.domain.Task
+import com.google.android.material.imageview.ShapeableImageView
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,5 +16,23 @@ fun TextView.completionTimestampToText(timestamp: Long) {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
         val date = Date(timestamp * 1000)
         text = dateFormat.format(date)
+    }
+}
+
+@BindingAdapter("taskImageButtonVisibility")
+fun Button.buttonVisibility(task: Task?) {
+    visibility = if (task == null) {
+        View.INVISIBLE
+    } else {
+        View.VISIBLE
+    }
+}
+
+@BindingAdapter("taskImageViewVisibility")
+fun ShapeableImageView.imageViewVisibility(task: Task?) {
+    visibility = if (task == null) {
+        View.INVISIBLE
+    } else {
+        View.VISIBLE
     }
 }
