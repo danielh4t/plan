2c4c9d494289567c7db986f9da698fbe7fc8f7d6
danielh4t <danielh4t@gmail.com>
2023-05-10 17:40:09 +0200

    add user messages for invalid credential and weak password

--- app/src/main/java/app/stacq/plan/ui/auth/AuthFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/auth/AuthFragment.kt
@@ -12,6 +12,8 @@ import app.stacq.plan.R
 import app.stacq.plan.databinding.FragmentAuthBinding
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
@@ -59,7 +61,6 @@ class AuthFragment : Fragment() {
                             navController.popBackStack()
                         } else {
                             // If sign in fails, display a message to the user.
-
                             Toast.makeText(
                                 requireContext(),
                                 R.string.sign_up_failed,
@@ -108,17 +109,38 @@ class AuthFragment : Fragment() {
                         }
                     }
                     .addOnFailureListener { e ->
-                        Firebase.crashlytics.recordException(e)
-                        Toast.makeText(
-                            requireContext(),
-                            e.localizedMessage,
-                            Toast.LENGTH_SHORT
-                        ).show()
+
+                        when (e) {
+                            is FirebaseAuthWeakPasswordException -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    R.string.weak_password,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                            is FirebaseAuthInvalidCredentialsException -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    R.string.invalid_credentials,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                            else -> {
+                                Firebase.crashlytics.recordException(e)
+                                Toast.makeText(
+                                    requireContext(),
+                                    e.localizedMessage,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
                     }
             }
         }
     }
-
+    
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
