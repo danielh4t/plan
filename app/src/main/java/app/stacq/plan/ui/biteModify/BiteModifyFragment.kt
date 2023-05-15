99bdbafd61702784d68a3589a79d66b947a4d7a2
danielh4t <danielh4t@gmail.com>
2023-05-15 22:30:18 +0200

    refactor remove fab and use filled button to save modified bite

--- app/src/main/java/app/stacq/plan/ui/biteModify/BiteModifyFragment.kt
+++ app/src/main/java/app/stacq/plan/ui/biteModify/BiteModifyFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -21,7 +22,6 @@ import app.stacq.plan.data.source.remote.bite.BiteRemoteDataSourceImpl
 import app.stacq.plan.data.source.remote.task.TaskRemoteDataSourceImpl
 import app.stacq.plan.databinding.FragmentBiteModifyBinding
 import com.google.android.material.shape.MaterialShapeDrawable
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -89,11 +89,10 @@ class BiteModifyFragment : Fragment() {
             }
         }
 
-        binding.biteModifyFab.setOnClickListener { clickedView ->
+        binding.biteModifySaveButton.setOnClickListener {
             val name: String = binding.biteModifyNameEditText.text.toString().trim()
             if (name.isEmpty()) {
-                Snackbar.make(clickedView, R.string.bite_name_required, Snackbar.LENGTH_SHORT)
-                    .setAnchorView(clickedView)
+                Toast.makeText(requireContext(), R.string.bite_name_required, Toast.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
             }
